f99fc0ca9fd68516a061380705b7e523e3cb6cd6
Elliott <ellmill00@gmail.com>
2024-12-25 02:28:51 -0500

    Add convert_models.py
    Once given a path to '/starting_kit/ingestion_program' and
    '/data/public_data/input_data'

---
+++ convert_models.py
@@ -0,0 +1,38 @@
+from sys import path
+from os import makedirs
+from os.path import isdir
+
+program_dir = #<path to>/starting_kit/ingestion_program 
+assert isdir(program_dir)
+path.append(program_dir)
+
+from data_manager import DataManager 
+from scripts.ct_model import CTModel
+from tqdm import tqdm
+
+import torch
+
+# Setip data manager
+
+basename='task1_v4'
+
+input_dir= #<path to>/data/public_data/input_data'
+data_manager = DataManager(basename, input_dir)
+
+# Grab training data
+
+training_data = data_manager.load_training_data()
+
+# Make sure you have the correct  folder
+ 
+makedirs('pt_models', exist_ok=True)
+
+for model_name in tqdm(data_manager.model_ids[:20]):
+    
+    model = data_manager.load_model(model_name)
+
+    ct_model = CTModel(input_model=model,data_type='float32')
+
+    ct_model.to('cpu')
+
+    torch.save(ct_model,'./pt_models/{}.pth'.format(model_name))
